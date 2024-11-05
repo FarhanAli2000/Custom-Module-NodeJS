d78c720cf95ab68ba96ffd499dd6e1e304693fdc
Syed Farhan Ali <tofarhanali@gmail.com>
2024-11-05 12:15:04 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const mymodule=require("./module")
+console.log(mymodule.name);
+console.log("this is index.js");
---
+++ module.js
@@ -0,0 +1,16 @@
+
+
+const data = {
+    name: "Module Example",
+    description: "This is data from module.js"
+  };
+  
+//   module.exports ={
+//    d: data,
+//    name:"Farhan",
+// repo:"Github"
+// } 
+
+
+//You can also use like this 
+module.exports.name='saad'
